bcc607148a71d4ee315bcfdc52675540d864023a
Joshua Foti <48845911+Jfoti64@users.noreply.github.com>
2024-04-08 03:44:59 -0500

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+    publicPath: "auto",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+  devServer: {
+    hot: "only",
+    watchFiles: {
+      paths: ["src/", "public/**/*"],
+      options: {
+        usePolling: false,
+      },
+    },
+  },
+  devtool: "inline-source-map",
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+      filename: "index.html",
+      inject: "body",
+    }),
+  ],
+};
