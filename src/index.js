d6425293cbd9c8f7f743956ae18d82156c0b3e74
Jfoti64 <48845911+Jfoti64@users.noreply.github.com>
2024-04-08 04:22:15 -0500

    Set up basic html layout.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
+import "./style.css";
+import image1 from "../images/aveedibya-dey-wRyQocoKOBA-unsplash.jpg";
+import image2 from "../images/max-bender-8FdEwlxP3oU-unsplash.jpg";
+import image3 from "../images/muzammil-soorma-5eARmrl56u0-unsplash.jpg";
 
+document.getElementById("slide1").src = image1;
+document.getElementById("slide2").src = image2;
+document.getElementById("slide3").src = image3;
