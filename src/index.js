4b4e1105090858b4561627965e1030c3aff127a9
Jfoti64 <48845911+Jfoti64@users.noreply.github.com>
2024-04-08 04:48:09 -0500

    Display one image at a time in center of page.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import "./style.css";
 import image1 from "../images/aveedibya-dey-wRyQocoKOBA-unsplash.jpg";
-import image2 from "../images/max-bender-8FdEwlxP3oU-unsplash.jpg";
+import image2 from "../images/muzammil-soorma-ayV1mD3HGyg-unsplash.jpg";
 import image3 from "../images/muzammil-soorma-5eARmrl56u0-unsplash.jpg";
 
 document.getElementById("slide1").src = image1;
