82718bbb16f79d8621c5cc28c3653102be491d63
Jfoti64 <48845911+Jfoti64@users.noreply.github.com>
2024-04-08 20:31:44 -0500

    Add functionality to left slider button.

--- src/changeSlide.js
+++ src/changeSlide.js
@@ -1,21 +1,40 @@
 import { getCurrentSlide, setCurrentSlide } from "./currentSlide";
+import { imageDetails } from "./images";
 
 let pixelsRight = 0;
+const numOfSlides = imageDetails.length;
 
 function nextSlide() {
   const { currentSlide } = getCurrentSlide();
   const carouselSlides = document.getElementById("carouselSlides");
 
-  if (currentSlide >= 2) {
+  if (currentSlide >= numOfSlides - 1) {
     pixelsRight = 0;
     setCurrentSlide(0);
   } else {
     pixelsRight += 500;
     setCurrentSlide(currentSlide + 1);
   }
-  
+
   // Directly applying the pixelsRight value with "px" without converting to a string first
   carouselSlides.style.right = `${pixelsRight}px`;
 }
 
-export { nextSlide };
+function previousSlide() {
+  const { currentSlide } = getCurrentSlide();
+  const carouselSlides = document.getElementById("carouselSlides");
+
+  if (currentSlide === 0) {
+    console.log("gg");
+    pixelsRight = 500 * (numOfSlides - 1);
+    setCurrentSlide(numOfSlides - 1);
+  } else {
+    pixelsRight -= 500;
+    setCurrentSlide(currentSlide - 1);
+  }
+
+  // Directly applying the pixelsRight value with "px" without converting to a string first
+  carouselSlides.style.right = `${pixelsRight}px`;
+}
+
+export { nextSlide, previousSlide };
--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import "./style.css";
 import { initializeSlides } from "./displayImages";
-import { nextSlide } from "./changeSlide";
+import { nextSlide, previousSlide } from "./changeSlide";
 
 initializeSlides();
 
-// Create function that takes all images imported and creates a slide in the dom for each. Also track the length of each image and move the slider that far over for each nextSlide(). 
-// Could create an object for each image that is stored in an array. Each object has its length and its position in the array. Or have the array only store the lengths, since the 
-// images only move one at a time. 
+const nextBtn = document.getElementById('right');
+
+nextBtn.addEventListener('click', () => {
+    nextSlide();
+});
+
+const previousBtn = document.getElementById('left');
+
+previousBtn.addEventListener('click', () => {
+    previousSlide();
+});
