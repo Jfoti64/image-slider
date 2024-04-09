f62e40793dba701c8db92e31685455901af72603
Jfoti64 <48845911+Jfoti64@users.noreply.github.com>
2024-04-09 04:30:27 -0500

    Automatically advance slide every 5 seconds.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import "./style.css";
 import "./eventListeners";
 import initializeSlides from "./displayImages";
+import { nextSlide } from "./changeSlide";
 
 
 initializeSlides();
+
+const intervalId = setInterval(nextSlide, 5000);
